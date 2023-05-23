cf1175bbd960cfba73ac56cbcde04c907539d233
Derovse <sashamartunyk2004@gmail.com>
2023-05-23 22:21:40 +0300

    d
    d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAY_VIDEO = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(PLAY_VIDEO, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem(PLAY_VIDEO));
+
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackParent = document.querySelector('.feedback-form');
+const feedbackEmail = document.querySelector('input');
+const feedbackMessage = document.querySelector('textarea');
+const KEY_USER_EMAIL = 'email';
+const KEY_USER_MESSAGE = 'message';
+
+const updateOutput = () => {
+  feedbackEmail.value = localStorage.getItem(KEY_USER_EMAIL) || '';
+  feedbackMessage.value = localStorage.getItem(KEY_USER_MESSAGE) || '';
+};
+
+window.addEventListener('load', updateOutput);
+
+const saveFormValues = () => {
+  localStorage.setItem(KEY_USER_EMAIL, feedbackEmail.value);
+  localStorage.setItem(KEY_USER_MESSAGE, feedbackMessage.value);
+};
+
+feedbackParent.addEventListener('input', throttle(saveFormValues, 500));
+
+feedbackParent.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const emailValue = feedbackEmail.value;
+  const messageValue = feedbackMessage.value;
+
+  localStorage.clear();
+  feedbackEmail.value = feedbackMessage.value = '';
+
+  const formData = {
+    email: emailValue,
+    message: messageValue,
+  };
+  console.log(formData);
+});
